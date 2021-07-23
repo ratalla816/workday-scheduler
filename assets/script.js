2df0f58197c43093998379f82406368a979755fa
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-07-22 20:04:16 -0500

    fixing script

--- assets/script.js
+++ assets/script.js
@@ -1,65 +1,23 @@
-// Display today's day and date
+
+// Current day and date
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-// save clicklistener accesses jQuery values
-$(document).ready(function () {
-  $(".saveBtn").on("click", function () {
-    var text = $(this).val(".description")();
-    var time = $(this).attr("id");
-    // saves input to local storage
-    localStorage.setItem(time, text);
-  })
 
-  // If we SET it we have to GET it!
+
+
+
+  // Get data from local storage 
   $("#9").val(localStorage.getItem("hour9"));
-  $("#hour10 description").val(localStorage.getItem("hour10"));
-  $("#hour11 description").val(localStorage.getItem("hour11"));
-  $("#hour12 description").val(localStorage.getItem("hour12"));
-  $("#hour13 description").val(localStorage.getItem("hour13"));
-  $("#hour14 description").val(localStorage.getItem("hour14"));
-  $("#hour15 description").val(localStorage.getItem("hour15"));
-  $("#hour16 description").val(localStorage.getItem("hour16"));
-  $("#hour17 description").val(localStorage.getItem("hour17"));
-
-  })
-
-  function hourTracker() {
-    // get current number of hours.
-    var currentHour = moment().hour();
-    var timeBlock = [];
-    // var timeBlock = hour().description();
-console.log (currentHour);
-console.log (this);
-    // .past .present .future is in CSS for a Reason ...{
-    if (timeBlock < currentHour) {
-      $(this)("past");
-      $(this)("present");
-      $(this)("future");
-    };
-
-    if (timeBlock < currentHour) {
-      $(this)("past");
-      $(this)("present");
-      $(this)("future");
-    };
-
-    if (timeBlock === currentHour) {
-      $(this)("past");
-      $(this)("present");
-      $(this)("future");
-    }
-
-  }
-    hourTracker();
+  $("#10").val(localStorage.getItem("hour10"));
+  $("#11").val(localStorage.getItem("hour11"));
+  $("#12").val(localStorage.getItem("hour12"));
+  $("#13").val(localStorage.getItem("hour13"));
+  $("#14").val(localStorage.getItem("hour14"));
+  $("#15").val(localStorage.getItem("hour15"));
+  $("#16").val(localStorage.getItem("hour16"));
+  $("#17").val(localStorage.getItem("hour17"));
   
-
-  
-    
-
-    // function setColor(element, color) {
-    // element.style.backgroundColor = color;
+  timeTracker();
 
 
-
-  
